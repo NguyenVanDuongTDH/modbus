91084703dc36dbe6acd19ba3233ddc84eadfdd8c
NguyenVanDuongTDH <154052803+NguyenVanDuongTDH@users.noreply.github.com>
2024-03-12 20:47:01 +0700

    up

--- example/modbus_example.dart
+++ example/modbus_example.dart
@@ -5,8 +5,9 @@ import 'package:serial/serial.dart';
 
 Uint16List data = Uint16List(100);
 Future<void> main() async {
-  SerialServer serial = Serial.tcp(host, port)
+  SerialServer serial = SerialWindowsServerUSB("COM11");
   ModbusSlave slave = ModbusSlave.RTU(serial);
   slave.configHoldingRegisters(HoldingRegistersConfig.fromList(data));
   slave.bind();
 }
+
